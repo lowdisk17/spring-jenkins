add9723a0b38f0d373664bb359d3f3ec05ed4c72
ra.andasan <ra.andasan@gmail.com>
2024-03-22 22:06:25 +0800

    Initial commit

---
+++ src/test/java/com/jenkins/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
