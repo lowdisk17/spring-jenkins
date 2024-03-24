c10b37f847f66fd83f53f1b87c8bafab3cdf2973
ra.andasan <ra.andasan@gmail.com>
2024-03-24 20:46:37 +0800

    Updated

---
+++ src/main/java/com/jenkins/jenkins/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.jenkins.jenkins.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class MainController {
+
+  @GetMapping("/service")
+  public String getMethodName() {
+      return "Hello World!";
+  }
+  
+}
